27ae5edc953d83b2c9ce68f49b3d16213d002c03
Kagutsuchi84 <115561782+Mattys8423@users.noreply.github.com>
2024-11-30 20:16:32 +0100

    Fix Audio and Play
    fix audio in 2 scenes and remove a useless script for the sound + fix play button

--- Ultimate Square Circle/Assets/Scripts/Menu/sound.cs
+++ Ultimate Square Circle/Assets/Scripts/Menu/sound.cs
@@ -9,29 +9,30 @@ public class sound : MonoBehaviour
     [SerializeField] Slider soundSlider;
     [SerializeField] private TMP_Text textMeshPro;
 
+    private void Awake()
+    {
+        soundSlider.value = PlayerPrefs.GetFloat("Volume", 100f);
+        AudioListener.volume = soundSlider.value * 10;
+    }
+
     void Start()
     {
-        // Initialiser le volume avec la valeur sauvegardée ou une valeur par défaut
         soundSlider.value = PlayerPrefs.GetFloat("Volume", 100f);
         AudioListener.volume = soundSlider.value * 10;
 
-        // Mettre à jour le texte initialement
         UpdateText(soundSlider.value);
 
-        // Ajouter un listener pour mettre à jour le texte lorsque le Slider change de valeur
         soundSlider.onValueChanged.AddListener(UpdateText);
         soundSlider.onValueChanged.AddListener(ChangeVolume);
     }
 
     private void UpdateText(float value)
     {
-        // Mettre à jour le texte avec la valeur du soundSlider
         textMeshPro.text = value.ToString("0");
     }
 
     private void OnDestroy()
     {
-        // Supprimer le listener pour éviter des erreurs
         soundSlider.onValueChanged.RemoveListener(UpdateText);
     }
 
